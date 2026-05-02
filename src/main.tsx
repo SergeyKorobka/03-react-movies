5933a8b3c48451952b5bdbaf615e5a77e011d440
SergeyKorobka <s.korobka@goit.ua>
2026-05-02 21:23:17 +0300

    fx

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,6 @@ import { createRoot } from 'react-dom/client';
 import 'modern-normalize';
 import './index.css';
 import App from './components/App/App.tsx';
-import { getAllMovies } from './services/movieService.ts';
-
-getAllMovies();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
